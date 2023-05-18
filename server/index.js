487a2aa1d360b50fdf3e34674f000fd3b175970a
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-05-19 00:39:57 +0530

    created users list api

--- server/index.js
+++ server/index.js
@@ -1,12 +1,26 @@
 //MODULES
-
 const http = require("http");
 const fs = require("fs");
 
 const PORT = 8080;
 
+//USERSLIST DATA
+const users = [
+    "Hasanul Banna",
+    "Jinas",
+    "Akshay",
+    "Anshid",
+    "Ameen",
+    "Thabsheer",
+    "Shamil",
+    "Habeeb",
+    "jabir",
+    "Ramees",
+    "Faseen",
+    "Shijah",
+];
+
 const server = http.createServer((req, res) => {
-    console.log("entered");
     switch (req.url) {
         case "/":
             //REAADING HTML FILE
@@ -16,18 +30,18 @@ const server = http.createServer((req, res) => {
                 res.write(data);
                 res.end();
             });
-
             break;
+
         case "/users":
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.write("<h1>hello</h1>");
+            res.writeHead(200, { "Content-Type": "text/plain" });
+            res.write(JSON.stringify(users));
             res.end();
             break;
 
         default:
             "/";
             res.writeHead(200, { "Content-Type": "text/html" });
-            res.write("<h1>hello</h1>");
+            res.write("<h1>404 PAGE NOT FOUND</h1>");
             res.end();
             break;
     }
