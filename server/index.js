d1939f3243a5840ee05deb0a444893863d0f5fe0
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-05-19 00:32:06 +0530

    first page completed

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 //MODULES
 
 const http = require("http");
+const fs = require("fs");
 
 const PORT = 8080;
 
@@ -8,11 +9,16 @@ const server = http.createServer((req, res) => {
     console.log("entered");
     switch (req.url) {
         case "/":
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.write("<h1>hello</h1>");
-            res.end();
+            //REAADING HTML FILE
+            fs.readFile("index.html", (err, data) => {
+                if (err) throw err;
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.write(data);
+                res.end();
+            });
+
             break;
-        case "/":
+        case "/users":
             res.writeHead(200, { "Content-Type": "text/html" });
             res.write("<h1>hello</h1>");
             res.end();
